52bf4e2f3d9528b40460b0b500c3f07f8807f630
AlpDemir182 <hagunenjoyer7@gmail.com>
2024-11-03 12:41:22 +0100

    Add files via upload

---
+++ flappyball.py
@@ -0,0 +1,46 @@
+import pgzrun
+import random
+
+TITLE = "Flappy Ball"
+WIDTH = 800
+HEIGHT = 600
+gravity = 2000
+
+class Ball():
+    def __init__(self, initialx, initialy):
+        self.x = initialx
+        self.y = initialy
+        self.vx = 200
+        self.vy = 0
+        self.radius = 40
+    
+    def draw(self):
+        pos = (self.x, self.y)
+        screen.draw.filled_circle(pos, self.radius, "green")
+
+ball1 = Ball(50,100)
+
+def draw():
+    screen.clear()
+    ball1.draw()
+
+def update(dt):
+    uy = ball1.vy
+    ball1.vy+=gravity * dt
+    ball1.y+=(uy+ball1.vy) * 0.5 * dt
+    if ball1.y > HEIGHT - ball1.radius:
+        ball1.y = HEIGHT - ball1.radius
+        ball1.vy = -ball1.vy * 0.9
+
+    ball1.x += ball1.vx * dt
+    if ball1.x > WIDTH - ball1.radius or ball1.x < ball1.radius:
+        ball1.vx = -ball1.vx
+
+def on_key_down(key):
+    if key == keys.SPACE:
+        ball1.vy = -500
+
+
+
+pgzrun.go()
+
